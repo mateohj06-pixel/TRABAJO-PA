68f47854cfe0d68a798f744b9c2cc52790bb09e4
mateohj06-pixel <mateo.hj06@gmail.com>
2026-04-06 00:00:11 +0000

    Implementación del menú

--- main.cpp
+++ main.cpp
@@ -11,3 +11,15 @@ struct Producto {
 int main() {
     return 0;
 }
+int opcion;
+
+do {
+    cout << "\n--- MENU ---\n";
+    cout << "1. Registrar producto\n";
+    cout << "2. Calcular total de compra\n";
+    cout << "3. Mostrar productos registrados\n";
+    cout << "4. Salir\n";
+    cout << "Seleccione una opcion: ";
+    cin >> opcion;
+
+} while(opcion != 4);
